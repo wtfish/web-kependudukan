b62460d67ab5cf1931cb7371e62d6f5a9654629b
jevon-ap <jevonnetran@gmail.com>
2022-08-06 14:08:41 +0700

    login

--- web-kependudukan/resources/views/Auth/login.blade.php
+++ web-kependudukan/resources/views/Auth/login.blade.php
@@ -15,7 +15,7 @@
                                     @csrf
                                     @if ($message = Session::get('success'))
                                         <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                            <strong>Opss Something wrong!</strong> Email atau password ada yang salah
+                                            <strong>Opsss Something wrong!</strong> Email atau password ada yang salah
                                             <button type="button" class="btn-close" data-bs-dismiss="alert"
                                                 aria-label="Close"></button>
                                         </div>
